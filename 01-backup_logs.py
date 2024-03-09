f68932d8b8133aaa40c3acb3c17fab06f7f9b547
Chob1tzu <teixeira09896@gmail.com>
2024-03-09 20:55:41 -0300

    script improved

--- 01-backup_logs.py
+++ 01-backup_logs.py
@@ -1,93 +1,46 @@
 import os
 import time
 from datetime import datetime
-from sys import platform
+import platform
 
-
-# Delete old backups?
-clear_backups = False
-backup_location = "/media/backup"
+backup_directory = "./media/backup"
 backup_dirs = ["/etc"]
-backup_name_format = "%date%-%backupName%"
-date = time.strftime("%Y_%m_%d-%H_%M")
+backup_name_format = "%Y-%m-%d__%H:%M-backupName.tar.gz"
 start_time = time.time()
 
-class Logger:
-    file = None
-
-    def __init__(self, fileName):
-        self.file = open(fileName, "a")
-
-    def log(self, logLevel, message):
-        now = str(datetime.now())
-        self.file.write("[" + now + "] " + logLevel + ": " + message + " \n")
-        print("\n [" + now + "] " + logLevel + ": " + message + " \n")
-
-    def close_file(self): 
-        self.file.close()
-
 def os_check():
-    if platform.startswith('linux'):
-        return True
-    else:
-        return False
+    return platform.system() == 'Linux'
 
-def create_backup_location():
-    logger.log("INFO", "Creating backup directory...")
-    os.makedirs(backup_location)
+def create_backup_directory():
+    os.makedirs(backup_directory, exist_ok=True)
 
-
-def clear_backups():  
-    logger.log("INFO", "Cleaning backup directory...")  
-    for file in os.listdir(backup_location):
-        if os.path.isfile(file):
-            os.remove(file)
-        elif os.path.isdir(file):
-            os.rmdir(file)
-        else:
-            logger.log("ERROR", file + " can't be deleted.")
-    logger.log("SUCCESS", "Successfully deleted old backup files.")
-
-def get_file_name(name):  
-    file_name = backup_name_format
-    file_name = file_name.replace('%date%', date)
-    file_name = file_name.replace('%backupName%', name)
-
-    print(file_name)
-
-    return file_name
+def get_file_name(name):
+    return datetime.now().strftime(backup_name_format).replace('backupName', name)
 
 def backup():
-    logger.log("INFO", "Starting backup for " + str(len(backup_dirs)) + " directories...")
-    if len(backup_dirs) == 0:
-        logger.log("INFO", "No directories to backup")
+    if not backup_dirs:
+        print("No directories to backup")
     else:
-        count = 0
-        for directory in backup_dirs:
-            backup_file_name = get_file_name('backup_' + str(count + 1)) + '.tar.gz'  
-            count += 1
-            logger.log("INFO", "Starting backup for " + directory + "...")  
-            status = os.system("cd " + backup_location + " && tar -czvf " + backup_file_name + " " + directory)  
+        for count, directory in enumerate(backup_dirs, start=1):
+            backup_file_name = get_file_name(f'backup_{count}')
+            status = os.system(f"cd {backup_directory} && tar -czvf {backup_file_name} {directory}")
             if status == 0:
-                logger.log("SUCCESS", "Backup for " + directory + " was successful.")  
+                print(f"Backup for {directory} was successful.")
             else:
-                logger.log("ERROR", "Failed to backup " + directory + ".")  
-    logger.log("SUCCESS", "Elapsed time: " + str(time.time() - start_time))
+                print(f"Failed to backup {directory}.")
+    print(f"Elapsed time: {time.time() - start_time:.2f} seconds")
 
-
-logger = Logger("backup.log")
-if os_check():
-    if os.path.exists(backup_location):
-        if clear_backups:
-            clear_backups()
-        backup()
+def main():
+    if os_check():
+        if os.path.exists(backup_directory):
+            backup()
+        else:
+            print(f"{backup_directory} does not exist.")
+            create_backup_directory()
+            backup()
     else:
-        logger.log("ERROR", "Failed to create backup.")
-        logger.log("ERROR", backup_location + " does not exist.")
-        create_backup_location()
-        backup()
+        print("ERROR", "This script is only for Linux.")
+    print("INFO", "Script execution finished.")
 
-    logger.close_file()
-else:
-    logger.log("ERROR", "This script is only for Linux.")
-    logger.close_file()
+if __name__ == "__main__":
+    main()
--- 02-resource_monitor.py
+++ 02-resource_monitor.py
@@ -1,15 +1,16 @@
-
-from datetime import datetime
+import os
 import psutil
+from datetime import datetime
+
+LOGS_DIRECTORY = "./logs"
+LOGS_NAME_FORMAT = "%Y-%m-%d__%H:%M-resources.log"
 
 def get_system_resources():
     try:
-        psutil.cpu_percent(interval=1, percpu=True)
-        cpu_percent = psutil.cpu_percent()
+        cpu_percent = psutil.cpu_percent(interval=1)
         memory_percent = psutil.virtual_memory().percent
         disk_percent = psutil.disk_usage("/").percent
         timestamp = datetime.now().isoformat()
-        
         data = {
             "cpu": cpu_percent,
             "memory": memory_percent,
@@ -21,13 +22,35 @@ def get_system_resources():
         print(f"Error occurred while retrieving system resources: {e}")
         return None
 
-  
-def print_system_resources():
-    data = get_system_resources()
+def print_system_resources(data):
     if data:
         print("\n", data)
     else:
         print("Failed to retrieve system resources.")
-        
-        
-print_system_resources()
+
+def check_and_create_logs_directory():
+    os.makedirs(LOGS_DIRECTORY, exist_ok=True)
+
+def get_logs_name():
+    return datetime.now().strftime(LOGS_NAME_FORMAT)
+
+def store_data_logs(data):
+    try:
+        check_and_create_logs_directory()
+        filename = get_logs_name()
+        filepath = os.path.join(LOGS_DIRECTORY, filename)
+
+        with open(filepath, "a") as f:
+            f.write(
+                f"{data['timestamp']}, CPU: {data['cpu']}%, Memory: {data['memory']}%, Disk: {data['disk']}%\n")
+    except Exception as e:
+        print(f"Error occurred while storing data logs: {e}")
+
+def run():
+    data = get_system_resources()
+    if data:
+        store_data_logs(data)
+        print_system_resources(data)
+
+if __name__ == "__main__":
+    run()
